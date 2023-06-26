5cc41a35e2799a430da5e3eddd97c4989982a2c2
Martin <Martin@Martin>
2023-06-26 16:00:55 +0300

    Update controller methods to match routes

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,18 @@
 class RestaurantPizzasController < ApplicationController
+    skip_before_action :verify_authenticity_token, only: [:create]
+  
+  # POST /restaurant_pizzas
+  def create
+    restaurant_pizzas = RestaurantPizza.create(restaurant_pizzas_params)
+    render json: restaurant_pizzas ,except: [:created_at, :updated_at], status: :created
+  end
+  
+
+  private
+
+  def restaurant_pizzas_params
+    params.permit(:price, :pizza_id, :restaurant_id)
+  end
+
 end
+
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,44 @@
 class RestaurantsController < ApplicationController
-end
+    rescue_from ActiveRecord::RecordInvalid, with:  :render_unprocessable_entity
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response 
+    skip_before_action :verify_authenticity_token, only: [:destroy]
+
+    
+      # GET /restaurants
+      def index
+        restaurants = Restaurant.all
+        render json: restaurants, except: [:created_at, :updated_at], status: :ok 
+      end
+    
+      # GET /restaurants/:id
+      def show
+        restaurants = find_restaurants
+        
+          render json: restaurants, except: [:id, :created_at, :updated_at], status: :ok                 
+      end   
+      # DELETE /restaurants/:id
+      def destroy
+        restaurants = find_restaurants
+        render json: restaurants 
+        restaurants.destroy
+        head :no_content        
+      end
+    
+      private
+
+    def render_unprocessable_entity(invalid)
+      render json:{error: invalid.record.errors}, status: :unprocessable_entity
+    end
+            
+      def render_not_found_response
+    
+        render json: { error: "Restaurant not found" }, status: :not_found
+    
+      end
+    
+      def find_restaurants
+        Restaurant.find(params[:id])
+      end
+    
+    end
+
