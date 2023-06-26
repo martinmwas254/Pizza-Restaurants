2ec638576de9af331d696597e0613cf076bebd25
Martin <Martin@Martin>
2023-06-26 15:45:19 +0300

    Initial project setup

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :restaurant_pizzas
+  resources :pizzas
+  resources :restaurants
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
