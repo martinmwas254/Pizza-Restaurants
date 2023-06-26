216221ac2cef5e93aebf866f6b0ad7a6c10fb7cb
Martin <Martin@Martin>
2023-06-26 15:56:27 +0300

    Initialize restaurantpizza relationships

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,2 +1,4 @@
 class Pizza < ApplicationRecord
-end
+    has_many :restaurant_pizzas
+    has_many :restaurants, through: :restaurant_pizzas
+  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+    pizzas = Pizza.create(name:Faker::Food.dish,ingredients:Faker::Food.ingredient)
+    restaurants = Restaurant.create(name:Faker::Restaurant.name,address:Faker::Address.street_address)
+end
+    
+
+pizzas = Pizza.all
+restaurants = Restaurant.all
+
+
+10.times do
+    restaurant = restaurants.sample
+    pizza = pizzas.sample
+    price = Faker::Number.between(from: 1, to: 30)
+  
+    RestaurantPizza.create(price: price,restaurant: restaurant,pizza: pizza)
+end
